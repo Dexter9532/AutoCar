a24b1966fb23ac0fc7ede56297a113d10c6236d4
Joelryberg <joeryb0730@skola.goteborg.se>
2025-02-20 09:26:05 +0100

    la till 180 grader koden i en txt fil

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,47 @@
 #include <OPT3101.h>
 #include <Arduino.h>
 
-// Skapa ett sensorobjekt
-OPT3101 sensor;
+// Motor A (ansluten till M1 på L298N)
+int enA = 10;  // PWM-hastighet
+int in1 = 9;   // Riktning
+int in2 = 8;   // Riktning
 
 void setup() {
-    Serial.begin(9600);
-    Wire.begin();
+  // Sätt motorpinnar som OUTPUT
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+}
 
-    sensor.init();
-    sensor.configureDefault();
+void demoOne() {
+  // Kör motor A framåt med 200/255 hastighet
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  analogWrite(enA, 200);
+  delay(2000);
+}
+
+void demoTwo() {
+  // Accelerera motor A från 0 till full hastighet
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH); // Kör bakåt
+  for (int i = 0; i < 256; i++) {
+    analogWrite(enA, i);
+    delay(20);
+  }
+
+  // Bromsa ner från full fart till 0
+  for (int i = 255; i >= 0; --i) {
+    analogWrite(enA, i);
+    delay(20);
+  }
+
+  // Stanna motorn
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
 }
 
 void loop() {
-    sensor.startSample();  // Starta en ny mätning
-    delay(1);  // Vänta en kort stund
-
-    while (!sensor.isSampleDone()) {
-        delay(1);  // Vänta tills sensorn är klar
-    }
-
-    sensor.readOutputRegs();  // Läs ut mätdata
-
-    int distance = sensor.distanceMillimeters;  // Spara avståndet i en variabel
-    Serial.println(distance);  // Skriv ut avståndet
-
-    delay(1);  // Vänta innan nästa mätning
+  demoOne();
+  delay(1000);
 }
